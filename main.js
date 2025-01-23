28ca3bf9085186b941e2424a4e4c13e64c3b7edc
Felipe Coll <leonfelipecoll@gmail.com>
2025-01-23 16:54:26 -0300

    Apirest terminada con funcionalidad

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
-const urlApi = 'https://api.thecatapi.com/v1/images/search'
+const Api_url = 'https://api.thecatapi.com/v1/images/search'
 
-fetch(urlApi)
-    .then(response => response.json())
-    .then(data => {
-        const img = document.querySelector('img')
-        img.src = data[0].url
-    })
+    async function reload() {
+        const res = await fetch(Api_url)
+        const data = await res.json()
+      
+            const img = document.querySelector('img')
+            img.src = data[0].url
+        
+    }
