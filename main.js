b5597d1a5b8f5737e819a6f804df9aef24ff3ee9
Felipe Coll <leonfelipecoll@gmail.com>
2025-01-24 18:42:24 -0300

    Ejemplo terminado

--- main.js
+++ main.js
@@ -1,31 +1,116 @@
-const akey ='live_PWKdmjhGz8tJxx0BObyhWPGyLHARPJ0AHhbr3IkWzUfYG01OGnvN5JFVGFrv1ug4'
-const Api_url =`https://api.thecatapi.com/v1/images/search?limit=3&api_key=${akey}`
+const API_URL_RANDOM = ' https://api.thecatapi.com/v1/images/'
 
-/*
-Ejemplo de como usar queryparameters
+const LIMIT_RANDOM = 'search?limit=5'
 
-querystring = [
-    '?',
-    'limit=3',
-    '&order=Desc',
-].join('');
+const API_KEY = "live_AxdLngQtmaQJsTHJsunuuqWHm5cs1PN1N1pevU5u1LgxXliMphLMhcxkK1J0i6UX";
 
-const URL = `https://api.thecatapi.com/v1/images/search${querystring}`
+const API_KEY_TWO = "live_p8gZ9g54dCDnjsWwK3Mzm1PAcm7djNnbm4kb29Woc32pxyVVBcLv7ePEfXVfkLN0"
 
-*/
+const API_URL_FAVOURITES = "https://api.thecatapi.com/v1/favourites";
 
-    async function reload() {
-        const res = await fetch(Api_url)
-        const data = await res.json()
-      
-            const img1 = document.getElementById('img1')
-            const img2 = document.getElementById('img2')
-            const img3 = document.getElementById('img3')
-            
-            img1.src = data[0].url
-            img2.src = data[1].url
-            img3.src = data[2].url
-        
+
+const img = document.querySelectorAll('div.item > img')
+
+async function loadRamdomCats() {
+    const res = await fetch(`${API_URL_RANDOM}${LIMIT_RANDOM}`, {
+        headers: {
+            'x-api-key': API_KEY
+        }
+    })
+    const data = await res.json()
+    console.log(data)
+
+    const container = document.querySelector('.random'); // Obtener el contenedor por su ID
+    while (container.firstChild) { // Mientras haya un primer hijo en el contenedor
+        container.removeChild(container.firstChild); // Eliminar el primer hijo
     }
 
-    reload()
+
+    for (let i = 0; i < data.length; i++) {
+        let p = `<p><b>Id:</b>${data[i].id}</p>`;
+
+        document.querySelector('.container').innerHTML += `
+        <div class='item'>
+            ${p}
+            <img src=${data[i].url}><br>
+            <button id="${data[i].id}" class="addFavourite" onclick="addCatToFavourite(this.id)">Add Cat To Favourites</button>
+        </div>
+        `
+    }
+}
+
+
+loadRamdomCats()
+
+
+async function addCatToFavourite(id) {
+    console.log(id)
+    await fetch(API_URL_FAVOURITES, {
+        method: 'POST',
+        headers: {
+            'x-api-key': API_KEY,
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            image_id: `${id}`
+        })
+    })
+
+    loadFavouritesCats()
+}
+
+
+async function loadFavouritesCats() {
+    const res = await fetch(`${API_URL_FAVOURITES}`, {
+        headers: {
+            'x-api-key': API_KEY,
+        }
+    })
+    const response = await res.json()
+    console.log(response)
+
+
+    const container = document.querySelector('.favourites'); // Obtener el contenedor por su ID
+    while (container.firstChild) { // Mientras haya un primer hijo en el contenedor
+        container.removeChild(container.firstChild); // Eliminar el primer hijo
+    }
+
+    response.forEach(data => {
+
+        const elementExists = document.querySelector(`div[id="${data.image_id}"]`)
+
+        if (!elementExists) {
+            let p = `<p><b>Id:</b>${data.image.id}</p>`;
+
+            document.querySelector('.favourites').innerHTML += `
+            <div id="${data.image.id}" class='item'>
+                ${p}
+                <img src=${data.image.url}><br>
+                <button id="${data.id}" class="quitFavourite" onclick="deleteFavouriteCat(this.id)">Quit Cat To Favourites</button>
+            </div>
+        `
+        }
+
+    })
+}
+loadFavouritesCats()
+
+async function deleteFavouriteCat(id) {
+    const res = await fetch(`${API_URL_FAVOURITES}/${id}`, {
+        method: 'DELETE',
+        headers: {
+            'Content-Type': 'application/json',
+            "x-api-key": API_KEY,
+        }
+    })
+    const data = await res.json()
+    console.log(data)
+
+    if (res.status !== 200) {
+        spanError.innerHTML = "Hubo un error: " + res.status + data.message;
+    } else {
+        console.log('Michi eliminado de favoritos' + data.id)
+    }
+
+    loadFavouritesCats()
+}
